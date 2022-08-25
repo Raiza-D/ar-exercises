691152fa0a427e5a7d91c87141ca073f8d63d1d3
Raiza De Guzman <santos_dg7@hotmail.com>
2022-08-25 16:49:32 -0700

    Remove initial code above code creating new employees. Code placed in employee.rb and store.rb files respectively

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -10,14 +10,6 @@ puts "----------"
 
 # Your code goes here ...
 
-class Store
-  has_many :employees
-end
-
-class Employee
-  belongs_to :store
-end
-
 @store1.employees.create(first_name: "Raiza", last_name: "De Guzman", hourly_rate: 100)
 @store1.employees.create(first_name: "Alice", last_name: "Wonderland", hourly_rate: 60)
 @store1.employees.create(first_name: "Oprah", last_name: "Winfrey", hourly_rate: 150)
@@ -25,3 +17,9 @@ end
 @store2.employees.create(first_name: "Augustus", last_name: "Maximus", hourly_rate: 50)
 @store2.employees.create(first_name: "Marc", last_name: "Antony", hourly_rate: 60)
 @store2.employees.create(first_name: "Faustina", last_name: "Brevsky", hourly_rate: 70)
+
+puts "Store 1 employee count:"
+puts @store1.employees.count
+
+puts "Store 2 employee count:"
+puts @store2.employees.count
