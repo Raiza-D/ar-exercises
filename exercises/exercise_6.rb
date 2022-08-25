5b8a385d8033bbeaa5d7def03ff0698ea376a89d
Raiza De Guzman <santos_dg7@hotmail.com>
2022-08-25 13:37:23 -0700

    Add code to tie relationship between stores and employees tables. Create new employees for store 1 and store 2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,19 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+class Store
+  has_many :employees
+end
+
+class Employee
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Raiza", last_name: "De Guzman", hourly_rate: 100)
+@store1.employees.create(first_name: "Alice", last_name: "Wonderland", hourly_rate: 60)
+@store1.employees.create(first_name: "Oprah", last_name: "Winfrey", hourly_rate: 150)
+
+@store2.employees.create(first_name: "Augustus", last_name: "Maximus", hourly_rate: 50)
+@store2.employees.create(first_name: "Marc", last_name: "Antony", hourly_rate: 60)
+@store2.employees.create(first_name: "Faustina", last_name: "Brevsky", hourly_rate: 70)
