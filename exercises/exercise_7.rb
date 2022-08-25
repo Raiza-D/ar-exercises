1d297e0841c528030fc5b398043f3a7fb056b354
Raiza De Guzman <santos_dg7@hotmail.com>
2022-08-25 15:51:19 -0700

    Add validations for stores and employees models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,26 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, presence: true, numericality: true, length: { in: 40..200 }
+  validates :store_id, presence: true
+end
+
+class Store
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, presence: true, numericality: true, length: { minimum: 0 }
+  validate :must_carry_apparel
+
+  def must_carry_apparel
+    if !mens_apparel.present? || !womens_apparel.present?
+      errors.add("mens_apparel or womens_apparel, cannot be blank")
+    end
+  end
+end
+
+delta_store = Store.create(name: "Delta")
+delta_store.save
+
+delta_store.errors
